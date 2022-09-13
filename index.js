900f55ef98fa79ffdb3443cc254b69780a159603
Alberto <albertoarennass@gmail.com>
2022-09-13 18:29:08 +0200

    feat:users-relation

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const db = require("./configs/db");
 const errors = require("./errors/commons");
 const cors = require("cors");
 const options = require("./configs/cors");
 
 const app = express();
 
-app.use(cors(options));
-app.use(express.json());
-app.use(cookieParser());
+const main = async () => {
+  const db = await require("./configs/db");
 
-app.use("/", require("./services")(db));
-
-app.use((_, __, next) => {
-  next(errors[404]);
-});
-
-app.use(({ statusCode, error }, _, res, __) => {
-  res.status(statusCode).json({
-    success: false,
-    message: error.message,
+  app.use(cors(options));
+  app.use(express.json());
+  app.use(cookieParser());
+  
+  app.use("/", require("./services")(db));
+  
+  app.use((_, __, next) => {
+    next(errors[404]);
   });
-});
+  
+  app.use(({ statusCode, error }, _, res, __) => {
+    res.status(statusCode).json({
+      success: false,
+      message: error.message,
+    });
+  });
+  
+  app.listen(process.env.PORT, () =>
+    console.info("> listening at:", process.env.PORT)
+  );
+}
 
-app.listen(process.env.PORT, () =>
-  console.info("> listening at:", process.env.PORT)
-);
+main()
--- sql/creation.sql
+++ sql/creation.sql
@@ -10,12 +10,6 @@ DROP EXTENSION IF EXISTS "uuid-ossp";
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS users (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  first_name VARCHAR(20) NOT NULL,
-  email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS cities (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -35,6 +29,25 @@ CREATE TABLE IF NOT EXISTS day_time (
 CREATE TABLE IF NOT EXISTS levels (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   level VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  first_name VARCHAR(20) NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+    cities_id uuid REFERENCES cities
+      ON UPDATE CASCADE
+      ON DELETE SET NULL,
+  group_type_id uuid REFERENCES group_type
+      ON UPDATE CASCADE
+      ON DELETE SET NULL,
+  day_time_id uuid REFERENCES day_time
+      ON UPDATE CASCADE
+      ON DELETE SET NULL,
+  levels_id uuid REFERENCES levels
+      ON UPDATE CASCADE
+      ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS group_class (
